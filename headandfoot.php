57c55f6991269361a77ba1ec38c4bb8ca1059fdb
beeone09 <58930430+beeone09@users.noreply.github.com>
2021-02-07 17:38:19 +0100

    Add files via upload

---
+++ headandfoot.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="headandfoot.css">
+    <title>Mobilom</title>
+</head>
+<body>
+    <header>
+        <img class="logocar" src="voiturelogo.png" alt="logo_de_voiture">
+    
+
+        <nav>
+            <div class="nav">
+                <a class="active" href="">Accueil</a>
+                <a href="">Ventes</a>
+                <a href="">À propos</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+
+    </header>
+
+    <section>
+
+    </section>
+
+    <footer>
+        <div class="pieds_de_page">
+            <a href="">Mentions légales</a>
+            <a href="">Politique de confidentialité</a>
+            <a href="">CUGV</a>
+        </div>
+    </footer>
+
+</body>
+</html>
