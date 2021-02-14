62c17d787b406f916886590db54816ec5196626a
beeone09 <58930430+beeone09@users.noreply.github.com>
2021-02-14 17:19:51 +0400

    Add files via upload

---
+++ head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="head.css">
+    <title>Mobilom</title>
+</head>
+<body>
+    <header>
+        <div class = "container_nav">
+            <div class = "logo">
+                <img class="logocar" src="logo_tangue.png" alt="logo_de_voiture">
+                <span class="text_logo">TANGU'OBILE</span>
+            </div>
+
+            <nav>
+                <div class="nav">
+                    <a class="active" href="">Accueil</a>
+                    <a href="">Ventes</a>
+                    <a href="">À propos</a>
+                    <a href="">Contact</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section>
+
+    </section>
+
+    <!--<footer>
+        <div class="pieds_de_page">
+            <a href="">Mentions légales</a>
+            <a href="">Politique de confidentialité</a>
+            <a href="">CUGV</a>
+        </div>
+    </footer>-->
+
+</body>
+</html>
