95ba53faff0989d055f014be4cac38733a697861
beeone09 <58930430+beeone09@users.noreply.github.com>
2021-02-16 13:17:33 +0100

    Update head.php

--- head.php
+++ head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="head.css">
+    <script type="text/javascript" src="head_java.js"></script> 
     <title>Mobilom</title>
 </head>
 <body>
