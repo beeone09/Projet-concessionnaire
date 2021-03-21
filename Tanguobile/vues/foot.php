507fa96471fdb1cca8670c135fbf64b5e686300d
beeone09 <58930430+beeone09@users.noreply.github.com>
2021-03-21 19:19:31 +0400

    Add files via upload

---
+++ Tanguobile/vues/foot.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../media/css/footer.css">
+    <title>Footer</title>
+</head>
+<body>
+    <footer>
+        <div class="pieds_de_page">
+            <span class="signature">@TANGU'OBILE</span>
+            <a class = "link_foot" href="mention.php">Mentions légales</a>
+            <a class = "link_foot" href="rgpd.php">Politique de confidentialité</a>
+            <a class = "link_foot" href="cugv.php">CUGV</a>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ Tanguobile/vues/rgpd.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../media/css/container_rgpd.css">
+    <link type="text/css" rel="stylesheet" href= "../media/css/nav.css">
+    <link type="text/css" rel="stylesheet" href= "../media/css/footer.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include('../vues/nav.php');?>
+
+    <div class = "container_rgpd">
+        <h1 class="titre1">Conditions de service</h1>
+
+        <h2 class="titre2">Informations et exclusion</h2>
+
+        <p>Les données présentes sur le présent formulaire sont sauvegardées dans un fichier informatisé par 
+            [identité de l’organisme responsable de traitement] dont le délégué à la protection des données est 
+            [renseignements personnels concernant le délégué : nom, adresses, et coordonnées électroniques et téléphoniques] pour 
+            [finalité du traitement]. Le traitement est effectué sur la base des intérêts légitimes de la 
+            société dans la mesure où [intérêt légitime du traitement].</p> <br/>
+
+        <p>Les renseignements rassemblés seront communiqués aux seuls destinataires suivants [destinataires des données]. 
+            Ces données feront l’objet de traitement dans notre département commercial et seront transmises à certains de nos 
+            prestataires. À noter que ces fichiers pourront être envoyés à nos filiales qui sont localisées à [pays de délocalisation] 
+            avec lesquelles nous avons signé des clauses contractuelles de confidentialité. (Ce dernier paragraphe ne sera mentionné que si 
+            un transfert des données vers les pays hors de l’UE a eu lieu).</p> <br/>
+
+        <p>Vos informations seront conservées pour un délai de [durée de conservation]/ La condition de conservation de vos données se 
+            feront dans un délai déterminé par les critères suivants [critères permettant de déterminer la durée de conservation].</p> <br/>
+
+        <p>Vous garderez un droit de regard sur vos informations. Vous pourriez y accéder, 
+            les rectifier, demander leur modification et suppression et exercer votre droit à la 
+            limitation du traitement. Vous pourrez exercer ces droits à tout moment. 
+            Pour cela, vous pourriez contacter notre Service juridique :</p> <br/>
+        
+        <ul>
+            <li>Par mail au : [adresse électronique du service juridique] .</li>
+            <li>Par courrier postal à l’adresse [adresse postale complète du siège social de la société].</li>
+        </ul> <br/>
+
+        <p>Aussi, vous pouvez retirer votre consentement au traitement de vos données à 
+            tout moment ou vous opposer au traitement de vos données. 
+            Vous pouvez également exercer un droit à la 
+            portabilité sur vos données personnelles.</p> <br/>
+
+        <p>Si après avoir contacté notre service juridique, vous constatez que vos droits 
+            « Informatiques et Libertés » ne sont pas respectés, 
+            vous pourrez adresser une réclamation à la Commission nationale de 
+            l’Informatique et des Libertés ou CNIL, 3 Places de Fontenoy, 75007 Paris.</p> <br/>
+
+        <p>Note : Les informations marquées d’astérisques sont obligatoires et 
+            doivent impérativement être fournies. 
+            Il s’agit de renseignements nécessaires à la 
+            fourniture des services ou la conclusion du contrat.</p> <br/>
+
+        <p>(Dans le cas d’une décision automatisée, le paragraphe suivant sera nécessairement inséré dans la déclaration) : 
+            vous êtes informé que les données [type de donnée] feront l’objet d’un traitement automatique permettant de 
+            prendre des décisions équitables et judicieuses. Vous pouvez toutefois révoquer cette décision automatique 
+            en vous adressant au délégué à la protection des données.</p>
+
+        <h2 class="titre2">Mettre les mentions minimales d’information sur deux supports différents</h2>
+
+        <p>La société [responsable de traitement] représentée par le délégué à la protection des données 
+            [renseignements concernant celui-ci si la société en désigné un] traite les 
+            données collectées pour [finalités du traitement].</p> <br/>
+
+        <p>Pour en savoir plus sur la gestion et les formalités de conservations et de 
+            traitement de vos données personnelles et pour exercer vos droits, 
+            reportez-vous à la notice ci-jointe ou cliquez ici [lien qui mène vers les 
+            clauses plus détaillées]. (Les informations supplémentaires pourront 
+            présenter de diverses manières ou sous forme de texte suivant le modèle précédent).</p>
+
+        <h2 class="titre2">Quid des modifications ?</h2>
+
+        <p>L’organisme responsable de la collecte, du traitement et de la conservation des 
+            données personnelles est libre de modifier le contenu de sa politique de 
+            confidentialité suivant l’évolution de ses activités. 
+            Il est toutefois tenu de notifier à son client les modifications effectuées sur 
+            la déclaration. Celui-ci sera ensuite libre d’appliquer ses droits de limitation et 
+            d’effacement de ses renseignements personnels ou même d’interrompre le contrat si 
+            les modifications ne lui conviennent pas.</p> <br/>
+    </div>
+
+    <?php include('../vues/foot.php');?>
+</body>
+</html>
